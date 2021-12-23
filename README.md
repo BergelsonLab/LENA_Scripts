559dd8fc6b751c03b3fdcf028236ad9ac0589804
erinecampbell <eec35@duke.edu>
2021-12-23 11:57:11 -0500

    re-ran script with new LENA files

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # LENA_Scripts
-Scripts for Erin (and whoever else for working with LENA files)
 
-There are two .r  scripts in here. One helps get stuff out of your its files (``functions.r``) and one helpsy ou  concatenate that for a directory of its files (``getting_lena_details_from_its``)
+These come from https://github.com/BergelsonLab/LENA_Scripts. Modified slightly to work with VIHI folder structure.
+3/9/2021 -- EC needs to fork/branch repo
+
+There are two .r  scripts in here. One helps get stuff out of your its files (``functions.r``) and one helps you  concatenate that for a directory of its files (``getting_lena_details_from_its``)
+
+Data file contains the list of .its files to run the script over. 
+
+Output .csv lives in SubjectFiles/LENA.
--- getting_lena_details_from_its.R
+++ getting_lena_details_from_its.R
@@ -1,35 +1,41 @@
 # some  code for reading in LENA variables
 # this script goes through each its library and creates the file-level
 #summaries for our speaker vars
+# 
 
 #note: this can take a LONG TIME to run over lots of files
 
-# this script expects a folder of its files, and a functions script that's in this LENA_script repo
-#  you  probably will need to change  path names and change corpusname to VIHI or whatever
-# throughouth  and in the writeout step
 
 # read in its folders -----------------------------------------------------
 source("functions.R")
 
 # corpusname ---------------------------------------------------------------------
+VIHI_participant_IDs <- read_csv("/Volumes/pn-opus/VIHI/SubjectInformation/General/VIHI_participant_IDs.csv") %>%
+  rename(sub_id=new_name) %>%
+  select(sub_id, match, match_type) 
 
-folder <- "../corpusname/its/"
-da <- as.data.frame(list.files(folder))
-names(da) <- c("its_file")
+folder <- "/Volumes/pn-opus/VIHI/SubjectFiles/LENA/"
+da <- read.csv("data/VIHITD_its_files.csv")
 da <- da %>%
-  mutate(its_path = paste(folder, as.character(its_file), sep=''))
+  mutate(its_path = paste(folder, as.character(sigroup), "/", as.character(sub), "/",
+                          as.character(id), "/", as.character(id), ".its", sep=''))
 
-corpusname_allspkr <- da %>% 
+
+VIHI_allspkr <- da %>% 
   group_by(its_path) %>% 
   do(get_its_tier_times(.$its_path)) %>% 
-  mutate(corpus = "corpusname")
+  mutate(corpus = "VIHI") 
 
-corpusname_chndetails <- da %>% 
+VIHI_chndetails <- da %>% 
   group_by(its_path) %>% 
   do(get_its_tier_times_CHN_details(.$its_path)) %>% 
-  mutate(corpus = "corpusname")
+  mutate(corpus = "VIHI")
 
-corpusname <- left_join(corpusname_allspkr  , corpusname_chndetails )
+VIHI_its_details <- left_join(VIHI_allspkr, VIHI_chndetails) %>% 
+  mutate(TV_dur = (TVN_dur + TVF_dur), 
+         adult_dur = (FAF_dur + FAN_dur + MAF_dur + MAN_dur), 
+         sub_id = str_sub(its_path, (nchar(its_path) - 13), (nchar(its_path)-4)),
+         group = str_sub(sub_id, 1, 2)) %>% 
+  left_join(VIHI_participant_IDs)
 
-
-write_csv(corpusname, "../project_folder_name/its_info_bycorpus/corpusname_itslengths.csv")
+write_csv(VIHI_its_details, "data/VIHI_its_details.csv")
